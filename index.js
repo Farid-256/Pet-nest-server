246368fc8f6c3f0bc49d17c44e686e7b55f1ddb8
Farid-256 <faridiqubal34fx@gmail.com>
2026-05-21 15:35:59 +0600

    last commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 })
 
 const JWKS = createRemoteJWKSet(
-    new URL('http://localhost:3000/api/auth/jwks')
+    new URL(`${CLIENT_URL}/api/auth/jwks`)
 )
 
 const verifyToken = async (req, res, next) => {
@@ -45,7 +45,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
     try {
-        await client.connect()
+        // await client.connect()
         const db = client.db('adopMe')
         const animalsCollection = db.collection('animals')
         const adoptingCollection = db.collection('adopCollection')
@@ -299,7 +299,7 @@ async function run() {
             }
         })
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
